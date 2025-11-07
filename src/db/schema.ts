15e1da14672864f7e69d447b2538dee473fa13ba
kapepa <kapepayx@gmail.com>
2025-11-07 20:24:14 +0200

    setup

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { timestamp, pgTable, uuid, varchar, text, uniqueIndex } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  clerkId: text("clerkId").unique().notNull(),
+  // name: varchar({ length: 255 }).notNull(),
+  name: text("name").notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  imageUrl: text("image_url").notNull(),
+  createdAt: timestamp("create_at").defaultNow().notNull(),
+  updateAt: timestamp("update_at").defaultNow().notNull(),
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
