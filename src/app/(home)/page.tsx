35777183f8ac9f8abde984a42d4af7ee3ac17055
kapepa <kapepayx@gmail.com>
2025-11-10 22:18:30 +0200

    setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { HydrateClient, trpc } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { PageClient } from "./client";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Karma" });
+
   return (
-    <div>
-      I will load videos in the feature!
-    </div>
+    <HydrateClient>
+      <Suspense
+        fallback={<p>Loading...</p>}
+      >
+        <ErrorBoundary
+          fallback={<p>Error...</p>}
+        >
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 };
