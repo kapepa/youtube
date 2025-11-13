722a4bcabdba88c743bb955c6e6a0397d7c1c652
kapepa <kapepayx@gmail.com>
2025-11-13 23:04:23 +0200

    infinity loading

---
+++ src/components/infinity-scroll.tsx
@@ -0,0 +1,53 @@
+import { useIntersectionObserver } from "@/hooks/use-intersection-observer";
+import { FC, useEffect } from "react";
+import { Button } from "./ui/button";
+
+interface InfinityScrollProps {
+  isManual?: boolean,
+  hasNextPage: boolean,
+  isFetchingNextPage: boolean,
+  fetchNextPage: () => void,
+}
+
+const InfinityScroll: FC<InfinityScrollProps> = (props) => {
+  const { isManual = false, hasNextPage, isFetchingNextPage, fetchNextPage } = props;
+  const { targetRef, isIntersection } = useIntersectionObserver({ threshold: 0.5, rootMargin: "100px" })
+
+  useEffect(() => {
+    if (isIntersection && hasNextPage && !isFetchingNextPage && !isManual) {
+      fetchNextPage();
+    }
+  }, [isIntersection, hasNextPage, isFetchingNextPage, isManual, fetchNextPage])
+
+  return (
+    <div
+      className="flex flex-col items-center gap-4 p-4"
+    >
+      <div
+        ref={targetRef}
+        className="h-1"
+      />
+      {
+        hasNextPage
+          ? (
+            <Button
+              variant="secondary"
+              disabled={!hasNextPage || isFetchingNextPage}
+              onClick={() => fetchNextPage()}
+            >
+              {isFetchingNextPage ? "Loading..." : "Load more"}
+            </Button>
+          )
+          : (
+            <p
+              className="text-sm text-muted-foreground"
+            >
+              You have reached the end of the list
+            </p>
+          )
+      }
+    </div>
+  )
+}
+
+export { InfinityScroll }
--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -3,7 +3,23 @@ import { VideosSection } from "../section/videos-section";
 
 const StudioView: FC = () => {
   return (
-    <div>
+    <div
+      className="flex flex-col gap-y-6 pt-2.5"
+    >
+      <div
+        className="px-4"
+      >
+        <h1
+          className="text-2xl font-bold"
+        >
+          Cannel content
+        </h1>
+        <p
+          className="text-xs text-muted-foreground"
+        >
+          Manage your channel content and videos
+        </p>
+      </div>
       <VideosSection />
     </div>
   )
