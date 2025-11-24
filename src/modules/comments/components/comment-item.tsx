f6fdd9c1e5c53584dc0b77741afe17d4147d574c
kapepa <kapepayx@gmail.com>
2025-11-24 12:05:44 +0200

    infinity loading comments

--- src/modules/comments/components/comment-item.tsx
+++ src/modules/comments/components/comment-item.tsx
@@ -4,13 +4,34 @@ import Link from "next/link";
 import { ROUTERS } from "@/lib/routers";
 import { UserAvatar } from "@/components/user-avatar";
 import { formatDistanceToNow } from "date-fns"
+import { trpc } from "@/trpc/client";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MessageSquareIcon, MoreVertical, Trash2Icon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 interface CommentItemProps {
-  comment: CommentsGetManyOutput[number],
+  comment: CommentsGetManyOutput["items"][number],
 }
 
 const CommentItem: FC<CommentItemProps> = (props) => {
   const { comment } = props;
+  const clerk = useClerk();
+  const { userId } = useAuth();
+  const utils = trpc.useUtils();
+  const remove = trpc.comments.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Comment deleted");
+      utils.comments.getMany.invalidate({
+        videoId: comment.videoId
+      })
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+      if (error.data?.code === "UNAUTHORIZED") clerk.openSignIn();
+    },
+  });
 
   return (
     <div>
@@ -53,6 +74,46 @@ const CommentItem: FC<CommentItemProps> = (props) => {
             {comment.value}
           </p>
         </div>
+        <DropdownMenu
+          modal={false}
+        >
+          <DropdownMenuTrigger
+            asChild
+          >
+            <Button
+              size="icon"
+              variant="ghost"
+              className="size-8"
+            >
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+          >
+            <DropdownMenuItem
+              onClick={() => { }}
+            >
+              <MessageSquareIcon
+                className="size-4"
+              />
+              Reply
+            </DropdownMenuItem>
+            {
+              comment.user.clerkId === userId
+              && (
+                <DropdownMenuItem
+                  onClick={() => remove.mutate({ id: comment.id })}
+                >
+                  <Trash2Icon
+                    className="size-4"
+                  />
+                  Delete
+                </DropdownMenuItem>
+              )
+            }
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   )
